655d6b94e3582cf911daeb4af9c37ed3c3de0427
Steven Lee <steven.lee1393@gmail.com>
2020-07-07 18:39:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,10 +16,13 @@ end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
+
 end
 
 def get_user_input
   # code #get_user_input here
+  user_input = gets.chomp
 end
 
 def end_game
